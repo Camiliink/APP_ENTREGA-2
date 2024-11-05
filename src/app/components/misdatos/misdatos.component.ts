ca479607d809006735d81fa5196ef8e81bbf7747
Bhiestt <je.santis@duocuc.cl>
2024-11-05 00:50:52 -0300

    actualizacion pequeña
    pequeños cambios

--- src/app/components/misdatos/misdatos.component.ts
+++ src/app/components/misdatos/misdatos.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { DatabaseService } from 'src/app/services/database.service';
 import { User } from 'src/app/model/user'; // Asegúrate de que el path sea correcto
 import { IonInput, IonButton, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,IonTextarea,IonGrid,IonRow,IonCol,IonIcon,IonCardContent, IonFab, IonFabButton, IonFabList} from '@ionic/angular/standalone'; 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { showToast } from 'src/app/tools/message-functions';
 
 
 @Component({
@@ -14,32 +16,21 @@ import { FormsModule } from '@angular/forms';
   imports: [IonHeader, IonToolbar, IonTitle, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonItem, IonLabel, IonInput, IonTextarea, IonGrid, IonRow, IonCol, IonButton, IonIcon, IonContent, IonCardContent, IonFab, IonFabButton, IonFabList,  CommonModule, FormsModule]
 })
 export class MisdatosComponent implements OnInit {
-  usuario: User; // Cambiado a User
+  usuario: User; 
   selectedImage: string | ArrayBuffer | null = null;
 
   constructor(private authService: AuthService) {
-    this.usuario = new User(); // Inicializar el usuario
+    this.usuario = new User(); 
   }
 
   async ngOnInit() {
-    const usuarioAlmacenado = await this.authService.readAuthUser(); // Asegúrate de que este método exista y retorne User o null
-    this.usuario = usuarioAlmacenado || new User(); // Cargar datos del usuario
+    const usuarioAlmacenado = await this.authService.readAuthUser();
+    this.usuario = usuarioAlmacenado || new User(); 
   }
 
   async guardarDatos() {
-    await this.authService.saveAuthUser(this.usuario); // Guarda el usuario en el servicio
-    // Aquí puedes agregar lógica adicional, como mostrar un mensaje de éxito
-  }
-
-  onImageSelected(event: Event) {
-    const file = (event.target as HTMLInputElement).files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.selectedImage = reader.result; // Muestra la imagen en miniatura
-      };
-      reader.readAsDataURL(file);
-    }
+    await this.authService.saveAuthUser(this.usuario); 
+    showToast('Datos guardados correctamente');
   }
 }
 
--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -11,7 +11,7 @@ export class User extends Person {
   password = '';
   secretQuestion = '';
   secretAnswer = '';
-  //db = inject(DataBaseService);
+  //db = inject(DatabaseService);
   image = '';
 
   constructor() {
