ec92d2f836e9adf67a18a200ab6b1e7c5e734816
alvidall <al.vidall@duocuc.cl>
2024-11-11 23:53:40 -0300

    Revert "qrfixordead"
    This reverts commit e7dcd0681de6877b98e9c81cc7b99e0de2231e02.

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -52,19 +52,14 @@ export class InicioPage {
   }
 
   async headerClick(button: string) {
-    if (button === 'testqr') {
+    if (button === 'testqr')
       this.showMiclaseComponent(Asistencia.jsonAsisExample);
-    }
-  
-    if (button === 'scan') {
-      if (Capacitor.getPlatform() === 'web') {
-        this.selectedComponent = 'codigoqr'; // En web, selecciona el componente de escaneo QR
-      } else {
-        // En móvil, escanea usando el servicio y muestra el componente de "miclase" si es válido
-        const qrData = await this.scanner.scan();
-        this.showMiclaseComponent(qrData);
-      }
-    }
+
+    if (button === 'scan' && Capacitor.getPlatform() === 'web')
+      this.selectedComponent = 'codigoqr'; // Cambié a 'codigoqr'
+
+    if (button === 'scan' && Capacitor.getPlatform() !== 'web')
+      this.showMiclaseComponent(await this.scanner.scan());
   }
 
   webQrScanned(qr: string) {
