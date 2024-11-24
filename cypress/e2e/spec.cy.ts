228a3aae941dbe1da2e79c5ef46a3c7c6842ac15
Camiliink <cam.jopia@duocuc.cl>
2024-11-23 22:49:37 -0300

    CYPRESS PRUEBAS

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -28,56 +28,26 @@ describe('Verificar mi aplicación', () => {
   //   });
   // })
 
-
-  //  it('Verificar publicación en foro', () => {
-  //    cy.visit('http://localhost:8100/ingreso').then(() => {
-  //      cy.contains('Sistema de asistencia Duoc UC');
-  //      cy.get('#correo').invoke('val', '');
-  //      cy.get('#correo').type('atorres');
-  //      cy.get('#password').invoke('val', '');
-  //      cy.get('#password').type('1234');
-  //      cy.contains('Ingresar').click();
-  //      cy.intercept('/inicio').as('route').then(() => {
-  //        cy.get('#saludo').contains('Sistema de asistencia Duoc UC');
-  //        cy.contains('foro').click({ force: true });
-
-  //         // Asegúrate de que el componente foro sea visible
-  //        cy.get('#foro-componente'); 
-  //        cy.get('#titulo').type(`Título de prueba ${numero}`);
-  //        cy.get('#contenido').type(`Contenido de prueba ${numero}`);
-  //        cy.contains('Guardar').click();
-  //        cy.wait(3000);
-  //        cy.contains('Aceptar').click();
-  //        cy.wait(3000);
-  //        cy.contains(`Título de prueba ${numero}`).should('exist');
-  //        cy.contains('Cerrar sesión').click();
-  //      });
-  //    });
-  //  })
-
-  it('Verificar publicación en foro', () => {
-    cy.visit('http://localhost:8100/ingreso').then(() => {
-      cy.contains('Sistema de asistencia Duoc UC');
-      cy.get('#correo').invoke('val', '');
-      cy.get('#correo').type('atorres');
-      cy.get('#password').invoke('val', '');
-      cy.get('#password').type('1234');
-      cy.contains('Ingresar').click();
-      cy.intercept('/inicio').as('route').then(() => {
-        cy.get('#saludo').contains('Sistema de asistencia Duoc UC');
-        cy.wait(5000);
-        cy.get('ion-segment-button[value="foro"]').click();
-        cy.get('#titulo').type(`Título de prueba ${numero}`);
-        cy.get('#contenido').type(`Contenido de prueba ${numero}`);
-        cy.contains('Guardar').click();
-        cy.wait(3000);
-        cy.contains('Aceptar').click();
-        cy.wait(3000);
-        cy.contains(`Título de prueba ${numero}`).should('exist');
-        cy.contains('Cerrar sesión').click();
-      });
-    });
-  });
+  // it('Verificar publicación en foro', () => {
+  //   cy.visit('http://localhost:8100/ingreso').then(() => {
+  //     cy.contains('Sistema de asistencia Duoc UC');
+  //     cy.get('#correo').invoke('val', '');
+  //     cy.get('#correo').type('atorres');
+  //     cy.get('#password').invoke('val', '');
+  //     cy.get('#password').type('1234');
+  //     cy.contains('Ingresar').click();
+  //     cy.intercept('/inicio').as('route').then(() => {
+  //       cy.get('#saludo').contains('Sistema de asistencia Duoc UC');
+  //       cy.get('ion-segment-button[value="foro"]').click();
+  //       cy.get('#titulo').type(`Título de prueba ${numero}`);
+  //       cy.get('#contenido').type(`Contenido de prueba ${numero}`);
+  //       cy.contains('Guardar').click();
+  //       cy.wait(3000);
+  //       cy.contains(`Título de prueba ${numero}`).should('exist');
+  //       cy.contains('salir').click();
+  //     });
+  //   });
+  // });
   
 //     it(`Verificar eliminación en foro de la última publicación con el título que contiene ${numero}`, () => {
 //     cy.visit('http://localhost:8100/ingreso').then(() => {
